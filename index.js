d5a80c030d56c5822b27810abdbbc1eb04f9ad84
Antonio Rodríguez <musantrobeats+dev@gmail.com>
2021-09-18 13:33:56 +0200

    feat: exercise 3.15-3.16

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 require('dotenv').config()
 const express = require('express')
+const app = express()
 const morgan = require("morgan");
 const cors = require('cors');
 const Person = require('./models/person')
 
-const app = express()
-
-app.use(cors())
 app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:', request.path)
+  console.log('Body:', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
 
 morgan.token('body', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : '');
 
@@ -28,10 +48,16 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -60,6 +86,15 @@ app.post('/api/persons', (request, response) => {
   })
 
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
